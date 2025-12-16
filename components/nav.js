f7a61370e72d0e97c9aa534ec5aad7a7d2aa4672
murtaza1244 <murtaza@cardsiq.io>
2025-12-16 23:19:21 +0500

    nav updated

--- components/nav.js
+++ components/nav.js
@@ -22,25 +22,37 @@ navElement.innerHTML = `
         </button>
         <div class="nav-links">
           <ul>
-            <li>
-              <a href="About.html">About</a>
-            </li>
-            <li>
-              <a href="Rules.html">Rules</a>
-            </li>
-            <li>
-              <a href="registration.html">Register</a>
-            </li>
-            <li>
-              <a href="Season-Finale.html">Finale</a>
-            </li>
-             <li>
-              <a href="Contact.html">Contact</a>
-            </li>
-            <li class="logo-desktop">
-              <a class="logo" href="index.html" aria-label="TekkyFutbol Home">
-                <img src="images/logo.png" alt="TekkyFutbol Logo">
+            <li class="dropdown-menu">
+              <a href="#">
+                <span>Shop</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+                  <path fill="currentColor"
+                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
+                </svg>
               </a>
+              <!-- dropdown -->
+              <div class="nav-links-dropdown">
+                <ul>
+<!--                  <li>-->
+<!--                    <a href="All-tickets.html">Tickets</a>-->
+<!--                  </li>-->
+                  <li>
+                    <a href="Shop.html">Merchandise</a>
+                  </li>
+                  <li>
+                    <a href="Season-pass-checkout.html">Season Pass</a>
+                  </li>
+<!--                  <li>-->
+<!--                    <a href="Single-ticket-checkout.html">Single Ticket</a>-->
+<!--                  </li>-->
+                  <li>
+                    <a href="supporter-bundle-checkout.html">Supporter Bundle</a>
+                  </li>
+                  <li>
+                    <a href="Playoff-ticket-checkout.html">Playoff Ticket</a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="dropdown-menu">
               <a href="#">
@@ -65,14 +77,25 @@ navElement.innerHTML = `
                       <a href="Standings.html">Standings</a>
                   </li>
                   <li>
-                    <a href="Next.html">Playoff Bracket</a>
+                    <a href="Next.html">Next Fixtures</a>
                   </li>
                 </ul>
               </div>
             </li>
+          
+            <li>
+              <a href="About.html">About</a>
+            </li>
+             <li class="logo-desktop">
+              <a class="logo" href="index.html" aria-label="TekkyFutbol Home">
+                <img src="images/logo.png" alt="TekkyFutbol Logo">
+              </a>
+            </li>
             <li class="dropdown-menu">
               <a href="#">
-                <span>Shop</span>
+                <span>
+                  Partners
+                </span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
                   <path fill="currentColor"
                     d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
@@ -82,69 +105,71 @@ navElement.innerHTML = `
               <div class="nav-links-dropdown">
                 <ul>
                   <li>
-                    <a href="All-tickets.html">Tickets</a>
+                    <a href="Partners.html">Partners</a>
                   </li>
                   <li>
-                    <a href="Shop.html">Merchandise</a>
-                  </li>
-                  <li>
-                    <a href="Season-pass-checkout.html">Season Pass</a>
-                  </li>
-                  <li>
-                    <a href="Single-ticket-checkout.html">Single Ticket</a>
-                  </li>
-                  <li>
-                    <a href="supporter-bundle-checkout.html">Supporter Bundle</a>
-                  </li>
-                  <li>
-                    <a href="Playoff-ticket-checkout.html">Playoff Ticket</a>
+                    <a href="Partners.html">Creating one</a>
                   </li>
                 </ul>
               </div>
             </li>
-            <li class="dropdown-menu">
-              <a href="#">
-                <span>MEDIA</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
-                  <path fill="currentColor"
-                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
-                </svg>
-              </a>
-              <!-- dropdown -->
-              <div class="nav-links-dropdown">
-                <ul>
-                  <li>
-                    <div class="nav-link coming-soon">Media & Highlights</div>
-                  </li>
+           
+<!--            <li>-->
+<!--              <a href="Rules.html">Rules</a>-->
+<!--            </li>-->
+            <li>
+              <a href="registration.html">Register</a>
+            </li>
+            <li>
+              <a href="Season-Finale.html">Finale</a>
+            </li>
+             <li>
+              <a href="Contact.html">Contact</a>
+            </li>
+            
+<!--            <li class="dropdown-menu">-->
+<!--              <a href="#">-->
+<!--                <span>MEDIA</span>-->
+<!--                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">-->
+<!--                  <path fill="currentColor"-->
+<!--                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />-->
+<!--                </svg>-->
+<!--              </a>-->
+<!--              &lt;!&ndash; dropdown &ndash;&gt;-->
+<!--              <div class="nav-links-dropdown">-->
+<!--                <ul>-->
 <!--                  <li>-->
-<!--                    <a href="Hall-of-fame.html">Hall of Fame</a>-->
+<!--                    <div class="nav-link coming-soon">Media & Highlights</div>-->
 <!--                  </li>-->
-                </ul>
-              </div>
-            </li>
-            <li class="dropdown-menu">
-              <a href="#">
-                <span>Legal</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
-                  <path fill="currentColor"
-                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
-                </svg>
-              </a>
-              <!-- dropdown -->
-              <div class="nav-links-dropdown">
-                <ul>
-                  <li>
-                    <a href="privacy-and-terms.html#privacy">Privacy Policy</a>
-                  </li>
-                  <li>
-                    <a href="privacy-and-terms.html#terms">Terms & Conditions</a>
-                  </li>
-                  <li>
-                    <a href="Refund.html">Refund Policy</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
+<!--&lt;!&ndash;                  <li>&ndash;&gt;-->
+<!--&lt;!&ndash;                    <a href="Hall-of-fame.html">Hall of Fame</a>&ndash;&gt;-->
+<!--&lt;!&ndash;                  </li>&ndash;&gt;-->
+<!--                </ul>-->
+<!--              </div>-->
+<!--            </li>-->
+<!--            <li class="dropdown-menu">-->
+<!--              <a href="#">-->
+<!--                <span>Legal</span>-->
+<!--                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">-->
+<!--                  <path fill="currentColor"-->
+<!--                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />-->
+<!--                </svg>-->
+<!--              </a>-->
+<!--              &lt;!&ndash; dropdown &ndash;&gt;-->
+<!--              <div class="nav-links-dropdown">-->
+<!--                <ul>-->
+<!--                  <li>-->
+<!--                    <a href="privacy-and-terms.html#privacy">Privacy Policy</a>-->
+<!--                  </li>-->
+<!--                  <li>-->
+<!--                    <a href="privacy-and-terms.html#terms">Terms & Conditions</a>-->
+<!--                  </li>-->
+<!--                  <li>-->
+<!--                    <a href="Refund.html">Refund Policy</a>-->
+<!--                  </li>-->
+<!--                </ul>-->
+<!--              </div>-->
+<!--            </li>-->
           </ul>
         </div>
       </nav>
