0eb6970d15d4080c20b6597bbdb101cd01ebcaeb
murtaza1244 <murtaza@cardsiq.io>
2025-12-17 12:15:07 +0500

    new changes

--- components/nav.js
+++ components/nav.js
@@ -23,28 +23,22 @@ navElement.innerHTML = `
         <div class="nav-links">
           <ul>
             <li class="dropdown-menu">
-              <a href="#">
+              <div class="nav-dropdown-trigger">
                 <span>Shop</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
                   <path fill="currentColor"
                     d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
                 </svg>
-              </a>
+              </div>
               <!-- dropdown -->
               <div class="nav-links-dropdown">
                 <ul>
-<!--                  <li>-->
-<!--                    <a href="All-tickets.html">Tickets</a>-->
-<!--                  </li>-->
                   <li>
                     <a href="Shop.html">Merchandise</a>
                   </li>
                   <li>
                     <a href="Season-pass-checkout.html">Season Pass</a>
                   </li>
-<!--                  <li>-->
-<!--                    <a href="Single-ticket-checkout.html">Single Ticket</a>-->
-<!--                  </li>-->
                   <li>
                     <a href="supporter-bundle-checkout.html">Supporter Bundle</a>
                   </li>
@@ -55,7 +49,7 @@ navElement.innerHTML = `
               </div>
             </li>
             <li class="dropdown-menu">
-              <a href="#">
+              <div class="nav-dropdown-trigger">
                 <span>
                   Schedule
                 </span>
@@ -63,7 +57,7 @@ navElement.innerHTML = `
                   <path fill="currentColor"
                     d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
                 </svg>
-              </a>
+              </div>
               <!-- dropdown -->
               <div class="nav-links-dropdown">
                 <ul>
@@ -83,16 +77,41 @@ navElement.innerHTML = `
               </div>
             </li>
           
-            <li>
-              <a href="About.html">About</a>
+            <li class="dropdown-menu">
+              <div class="nav-dropdown-trigger">
+                <span>About</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+                  <path fill="currentColor"
+                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
+                </svg>
+              </div>
+              <!-- dropdown -->
+              <div class="nav-links-dropdown">
+                <ul>
+                  <li>
+                    <a href="About.html">About TekkyFutbol</a>
+                  </li>
+                  <li>
+                    <a href="Rules.html">Rules</a>
+                  </li>
+                  <li>
+                    <a href="Staff.html">Staff</a>
+                  </li>
+                  <li>
+                    <div class="disabled">Hall of Fame</div>
+                  </li>
+                </ul>
+              </div>
             </li>
+            
              <li class="logo-desktop">
               <a class="logo" href="index.html" aria-label="TekkyFutbol Home">
                 <img src="images/logo.png" alt="TekkyFutbol Logo">
               </a>
             </li>
+            
             <li class="dropdown-menu">
-              <a href="#">
+              <div class="nav-dropdown-trigger">
                 <span>
                   Partners
                 </span>
@@ -100,7 +119,7 @@ navElement.innerHTML = `
                   <path fill="currentColor"
                     d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />
                 </svg>
-              </a>
+              </div>
               <!-- dropdown -->
               <div class="nav-links-dropdown">
                 <ul>
@@ -113,10 +132,6 @@ navElement.innerHTML = `
                 </ul>
               </div>
             </li>
-           
-<!--            <li>-->
-<!--              <a href="Rules.html">Rules</a>-->
-<!--            </li>-->
             <li>
               <a href="registration.html">Register</a>
             </li>
@@ -126,50 +141,6 @@ navElement.innerHTML = `
              <li>
               <a href="Contact.html">Contact</a>
             </li>
-            
-<!--            <li class="dropdown-menu">-->
-<!--              <a href="#">-->
-<!--                <span>MEDIA</span>-->
-<!--                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">-->
-<!--                  <path fill="currentColor"-->
-<!--                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />-->
-<!--                </svg>-->
-<!--              </a>-->
-<!--              &lt;!&ndash; dropdown &ndash;&gt;-->
-<!--              <div class="nav-links-dropdown">-->
-<!--                <ul>-->
-<!--                  <li>-->
-<!--                    <div class="nav-link coming-soon">Media & Highlights</div>-->
-<!--                  </li>-->
-<!--&lt;!&ndash;                  <li>&ndash;&gt;-->
-<!--&lt;!&ndash;                    <a href="Hall-of-fame.html">Hall of Fame</a>&ndash;&gt;-->
-<!--&lt;!&ndash;                  </li>&ndash;&gt;-->
-<!--                </ul>-->
-<!--              </div>-->
-<!--            </li>-->
-<!--            <li class="dropdown-menu">-->
-<!--              <a href="#">-->
-<!--                <span>Legal</span>-->
-<!--                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">-->
-<!--                  <path fill="currentColor"-->
-<!--                    d="M18.53 9.53a.75.75 0 0 0 0-1.06H5.47a.75.75 0 0 0 0 1.06l6 6a.75.75 0 0 0 1.06 0z" />-->
-<!--                </svg>-->
-<!--              </a>-->
-<!--              &lt;!&ndash; dropdown &ndash;&gt;-->
-<!--              <div class="nav-links-dropdown">-->
-<!--                <ul>-->
-<!--                  <li>-->
-<!--                    <a href="privacy-and-terms.html#privacy">Privacy Policy</a>-->
-<!--                  </li>-->
-<!--                  <li>-->
-<!--                    <a href="privacy-and-terms.html#terms">Terms & Conditions</a>-->
-<!--                  </li>-->
-<!--                  <li>-->
-<!--                    <a href="Refund.html">Refund Policy</a>-->
-<!--                  </li>-->
-<!--                </ul>-->
-<!--              </div>-->
-<!--            </li>-->
           </ul>
         </div>
       </nav>
@@ -213,7 +184,7 @@ if (menuTogglerBtn) {
 }
 
 // Open dropdown menu on mobile
-document.querySelectorAll('.dropdown-menu > a').forEach(trigger => {
+document.querySelectorAll('.dropdown-menu > .nav-dropdown-trigger').forEach(trigger => {
     trigger.addEventListener('click', function (e) {
         e.preventDefault();
 
